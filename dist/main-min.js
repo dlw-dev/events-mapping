c2912d9c11e975e8c0117dda0efc7c18dd7e6908
Emerson Garrido <emersongarrido@hotmail.com.br>
2021-04-07 19:25:10 -0400

    chore: news functions

--- dist/main-min.js
+++ dist/main-min.js
@@ -1,1 +1,1 @@
-async function Connection(n,t){return fetch(n,{method:"post",headers:{access_token:t,"Access-Control-Allow-Origin":"*"},body:JSON.stringify({test:1})}).then(n=>{n.json()}).then(n=>n)}
+async function Connection(n,t){return await fetch(n,{method:"post",body:JSON.stringify({test:1})}).then(n=>n.json()).then(n=>console.log(n))}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 async function Connection(host, access_token) {
 
-  const connect = fetch(host, {
+  const connect = await fetch(host, {
     method: 'post',
-    headers : {
-      "access_token": access_token,
-      "Access-Control-Allow-Origin" : "*"
-    },
+    // headers : {
+    //   "access_token": access_token,
+    //   "Access-Control-Allow-Origin" : "*"
+    // },
     body: JSON.stringify({test: 1})
   })
-  .then(response => {
-    response.json()
-  })
-  .then(data => {
-    return data
-  })
+  .then(response => response.json())
+  .then(data => console.log(data));
 
   return connect;
 }

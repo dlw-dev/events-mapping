796d6e15202d206eaed496fcc19e5438d68e0d73
Emerson Garrido <emersongarrido@hotmail.com.br>
2021-04-07 16:53:27 -0400

    chore: news functions

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
-async function dlweb(){
+const axios = require('axios');
 
-  async function connection(host, access_token){
+async function connection(host, access_token){
 
-    const initial = {
-      host: host,
-      access_token: access_token,
-    };
+  const initial = {
+    host: host,
+    access_token: access_token,
+  };
 
-    return initial
-  }
+  return initial
 
+}
+
+async function sendEvent(host, access_token){
+
+  headers = {'access_token': access_token}
+
+  const connect = await axios.post(host, headers, {
+    data : 'teste'
+  })
+
+  return connect
 }

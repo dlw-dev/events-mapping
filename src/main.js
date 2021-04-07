d34a0c729a43cbb9d85f7697bbe25fb6f413aa49
Emerson Garrido <emersongarrido@hotmail.com.br>
2021-04-07 17:28:30 -0400

    chore: news functions

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ async function Connection(host, access_token){
 
   const connect = await axios.post(host, {
     headers : {
-      'access_token': access_token
+      'access_token': access_token,
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     }
   }, { data : 'teste'});
 
